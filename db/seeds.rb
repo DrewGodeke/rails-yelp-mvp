c823475203c783c8021b69039b6b1ebfdc0caf36
andrew godeke <legodeke@hotmail.com>
2024-04-13 15:17:34 -0400

    controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,48 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# Clear existing records
+Restaurant.destroy_all
+
+# Array of restaurant attributes
+restaurants_attributes = [
+  {
+    name: "Restaurant 1",
+    address: "Address 1",
+    phone_number: "123-456-7890",
+    category: "italian",
+  },
+  {
+    name: "Restaurant 2",
+    address: "Address 2",
+    phone_number: "987-654-3210",
+    category: "french",
+  },
+  {
+    name: "Restaurant 3",
+    address: "Address 3",
+    phone_number: "456-789-0123",
+    category: "japanese",
+  },
+  {
+    name: "Restaurant 4",
+    address: "Address 4",
+    phone_number: "321-654-0987",
+    category: "chinese",
+  },
+  {
+    name: "Restaurant 5",
+    address: "Address 5",
+    phone_number: "789-012-3456",
+    category: "belgian",
+  }
+]
+
+# Create restaurants from attributes
+restaurants_attributes.each do |attributes|
+  restaurant = Restaurant.find_or_initialize_by(name: attributes[:name])
+  restaurant.address = attributes[:address]
+  restaurant.phone_number = attributes[:phone_number]
+  restaurant.category = attributes[:category]
+  restaurant.save!
+end
